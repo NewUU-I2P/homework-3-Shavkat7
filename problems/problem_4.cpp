b5cf1a0cb2a318d23f6c6718f8e5a6bcac64a04f
Shavkat Sadulloyev <s.sadulloyev@newuu.uz>
2023-11-10 13:08:45 +0500

    4

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,14 +1,15 @@
 #include <string>
 #include <sstream>
+using namespace std;
 
-std::string problemSolution4(const std::string &macAddress) {
+std::string problemSolution4(const std::string MA) {
     // write your code here
     std::string result;
-    if (&macAddress == "FF:FF:FF:FF:FF:FF") {
+    if (MA == "FF:FF:FF:FF:FF:FF") {
         result = "Broadcast";
-    } else if (long int(&macAddress[1]) % 2 == 0) {
+    } else if (MA[1] % 2 == 0) {
         result = "Unicast";
-    } else if (long int(&macAddress[1]) % 2 != 0) {
+    } else if (MA[1] % 2 != 0) {
         result = "Multicast";
     }
     // make use of control flow statements
